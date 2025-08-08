6e4da4e8ec23ac14f84a3a6ff0aec1c13b4b77b8
Yevhen Bobrov <yevhen.bobrov@gmail.com>
2025-08-09 00:54:27 +0300

    Parallel block

--- Simpipe.Net.Tests/Blocks/ParallelBlockFixture.cs
+++ Simpipe.Net.Tests/Blocks/ParallelBlockFixture.cs
@@ -5,94 +5,48 @@ public class ParallelBlockFixture
 {
     class TestItem
     {
-        public int Id = 0;
         public string Block1Value;
         public string Block2Value;
-    }
-
-    class CompletionTrackingExecutor<T> : IActionBlockExecutor<T>
-        where T : notnull
-    {
-        readonly ActionBlock<T> completion;
-        readonly Dictionary<T, int> completed = new();
-        readonly int count;
-        readonly Func<T, Task> done;
-        readonly IActionBlockExecutor<T> executor;
-
-        public CompletionTrackingExecutor(int count, Func<T, Task> done, IActionBlockExecutor<T> executor)
-        {
-            this.count = count;
-            this.done = done;
-            this.executor = executor;
-
-            completion = new ActionBlock<T>(capacity: 1, parallelism: 1, BlockItemAction<T>.Async(TrackDone));
-        }
-
-        async Task TrackDone(T item)
-        {
-            if (completed.TryGetValue(item, out var currentCount))
-                completed[item] = currentCount + 1;
-            else
-                completed[item] = 1;
-
-            if (completed[item] == count)
-                await done(item);
-        }
-
-        public Task ExecuteSend(IActionBlock<T> block, BlockItem<T> item, BlockItemAction<T> send) =>
-             executor.ExecuteSend(block, item, send);
-
-        public Task ExecuteAction(IActionBlock<T> block, BlockItem<T> item, BlockItemAction<T> action) =>
-            executor.ExecuteAction(block, item, action);
-
-        public async Task ExecuteDone(IActionBlock<T> block, BlockItem<T> item, BlockItemAction<T> done)
-        {
-            await executor.ExecuteDone(block, item, done);
-            await completion.Send(item);
-        }
     }
 
     [Test]
     public async Task Awaits_all_inner_blocks_before_reporting_done()
     {
         var item = new TestItem { Block1Value = "", Block2Value = "" };
-
         var doneItems = new List<TestItem>();
-
-        var executor = new CompletionTrackingExecutor<TestItem>(
-            count: 2,
-            done: x =>
-            {
-                doneItems.Add(x);
-                return Task.CompletedTask;
-            },
-            DefaultExecutor<TestItem>.Instance);
-
         var tcs1 = new TaskCompletionSource();
 
-        var innerBlock1 = new ActionBlock<TestItem>(
+        var parallelBlock = new ParallelBlock<TestItem>(
             capacity: 1,
-            parallelism: 1,
-            BlockItemAction<TestItem>.Async(i =>
+            blockCount: 2,
+            executor: DefaultExecutor<TestItem>.Instance,
+            done: item =>
             {
-                i.Block1Value = "1";
-                return tcs1.Task;
-            }),
-            executor: executor);
-
-        var innerBlock2 = new ActionBlock<TestItem>(
-            capacity: 1,
-            parallelism: 1,
-            BlockItemAction<TestItem>.Async(i =>
-            {
-                i.Block2Value = "2";
+                doneItems.Add(item);
                 return Task.CompletedTask;
-            }),
-            executor: executor);
+            },
+            blocksFactory: executor =>
+            [
+                new ActionBlock<TestItem>(
+                    capacity: 1,
+                    parallelism: 1,
+                    BlockItemAction<TestItem>.Async(async i =>
+                    {
+                        await Task.Delay(50);
+                        i.Block1Value = "1";
+                        await tcs1.Task;
+                    }),
+                    executor: executor),
 
-        await innerBlock1.Send(item);
-        await innerBlock2.Send(item);
-        await Task.Delay(10);
+                new ActionBlock<TestItem>(
+                    capacity: 1,
+                    parallelism: 1,
+                    BlockItemAction<TestItem>.Sync(i => i.Block2Value = "2"),
+                    executor: executor)
+            ]);
+
+        await parallelBlock.Send(item);
+        await Task.Delay(100);
 
         Assert.That(item.Block1Value, Is.EqualTo("1"));
         Assert.That(item.Block2Value, Is.EqualTo("2"));
--- Simpipe.Net/Blocks/ParallelBlock.cs
+++ Simpipe.Net/Blocks/ParallelBlock.cs
@@ -1,46 +1,86 @@
-using System.Threading.Channels;
+using System.Diagnostics;
 
 namespace Simpipe.Blocks;
 
 public class ParallelBlock<T> : IActionBlock<T>
 {
-    readonly Channel<BlockItem<T>> input;
+    readonly ActionBlock<T> input;
     readonly List<IActionBlock<T>> blocks;
-    readonly BlockItemAction<T> done;
-    readonly CancellationToken cancellationToken;
-    readonly Task processor;
+    readonly CompletionTrackingExecutor<T> completionTracker;
 
     public ParallelBlock(
         int capacity,
-        List<IActionBlock<T>> blocks,
-        BlockItemAction<T> done,
+        int blockCount,
+        IActionBlockExecutor<T> executor,
+        Func<T, Task> done,
+        Func<IActionBlockExecutor<T>, List<IActionBlock<T>>> blocksFactory,
         CancellationToken cancellationToken = default)
     {
-        this.blocks = blocks;
-        this.done = done;
-        this.cancellationToken = cancellationToken;
+        completionTracker = new CompletionTrackingExecutor<T>(blockCount, done, DefaultExecutor<T>.Instance);
 
-        input = Channel.CreateBounded<BlockItem<T>>(capacity);
-        processor = Task.Run(ProcessChannel, cancellationToken);
+        blocks = blocksFactory(completionTracker);
+        if (blocks.Count != blockCount)
+            throw new ArgumentException($"Expected {blockCount} blocks, but got {blocks.Count}.");
+
+        input = new ActionBlock<T>(
+            capacity: capacity,
+            parallelism: blockCount,
+            BlockItemAction<T>.Async(item => Task.WhenAll(blocks.Select(block => block.Send(item)))),
+            executor: executor,
+            cancellationToken: cancellationToken);
     }
 
-    public async Task Send(BlockItem<T> item) => await input.Writer.WriteAsync(item, cancellationToken);
+    public async Task Send(BlockItem<T> item) => await input.Send(item);
 
     public async Task Complete()
     {
-        input.Writer.Complete();
-        await processor;
-        
-        var completions = blocks.Select(block => block.Complete());
-        await Task.WhenAll(completions);
+        await input.Complete();
+        await Task.WhenAll(blocks.Select(block => block.Complete()));
+        await completionTracker.Complete();
+    }
+}
+
+internal class CompletionTrackingExecutor<T> : IActionBlockExecutor<T>
+{
+    readonly ActionBlock<T> completion;
+    readonly Dictionary<object, int> completed = new();
+    readonly int blockCount;
+    readonly Func<T, Task> done;
+    readonly IActionBlockExecutor<T> executor;
+
+    public CompletionTrackingExecutor(int blockCount, Func<T, Task> done, IActionBlockExecutor<T> executor)
+    {
+        this.blockCount = blockCount;
+        this.done = done;
+        this.executor = executor;
+
+        completion = new ActionBlock<T>(capacity: 1, parallelism: 1, BlockItemAction<T>.Async(TrackDone));
     }
 
-    async Task ProcessChannel()
+    async Task TrackDone(T item)
     {
-        await foreach (var item in input.Reader.ReadAllAsync(cancellationToken))
-        {
-            await Task.WhenAll(blocks.Select(block => block.Send(item)));
-            await done.Execute(item);
-        }
+        Debug.Assert(item != null, nameof(item) + " != null");
+
+        if (completed.TryGetValue(item, out var currentCount))
+            completed[item] = currentCount + 1;
+        else
+            completed[item] = 1;
+
+        if (completed[item] == blockCount)
+            await done(item);
     }
+
+    public Task ExecuteSend(IActionBlock<T> block, BlockItem<T> item, BlockItemAction<T> send) =>
+        executor.ExecuteSend(block, item, send);
+
+    public Task ExecuteAction(IActionBlock<T> block, BlockItem<T> item, BlockItemAction<T> action) =>
+        executor.ExecuteAction(block, item, action);
+
+    public async Task ExecuteDone(IActionBlock<T> block, BlockItem<T> item, BlockItemAction<T> done)
+    {
+        await executor.ExecuteDone(block, item, done);
+        await completion.Send(item);
+    }
+
+    public Task Complete() => completion.Complete();
 }
